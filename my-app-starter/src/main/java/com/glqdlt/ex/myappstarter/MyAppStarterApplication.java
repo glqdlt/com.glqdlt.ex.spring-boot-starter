0b456233a9f9c7f000678b2a8d6b87a1ea0fe216
glqdlt <dlfdnd0725@gmail.com>
2019-04-14 15:29:12 +0900

    init app

---
+++ my-app-starter/src/main/java/com/glqdlt/ex/myappstarter/MyAppStarterApplication.java
@@ -0,0 +1,13 @@
+package com.glqdlt.ex.myappstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAppStarterApplication.class, args);
+	}
+
+}
---
+++ my-app-starter/src/test/java/com/glqdlt/ex/myappstarter/MyAppStarterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.glqdlt.ex.myappstarter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyAppStarterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
