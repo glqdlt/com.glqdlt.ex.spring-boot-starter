76d86418bb078342833751a7d079e556de5b6bef
glqdlt <dlfdnd0725@gmail.com>
2019-04-14 15:30:40 +0900

    구현

--- my-app-starter/src/main/java/com/glqdlt/ex/myappstarter/MyAppStarterApplication.java
+++ my-app-starter/src/main/java/com/glqdlt/ex/myappstarter/MyAppStarterApplication.java
@@ -1,13 +1,24 @@
 package com.glqdlt.ex.myappstarter;
 
+import com.glqdlt.ex.springbootstarter.MySetup;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyAppStarterApplication {
+public class MyAppStarterApplication implements ApplicationRunner{
+
+	@Autowired
+	private MySetup mySetup;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyAppStarterApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println(mySetup.toString());
+	}
 }
